98a5e511f9baf4b17650c330212b48bc79ab9c76
Doug <akachi.sonne@gmail.com>
2022-03-19 17:31:22 -0500

    added "where" function to locate value with n-dim coord

--- src/array.hpp
+++ src/array.hpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<array>
 #include<vector>
+#include<stdexcept>
 
 template<typename T>
 class Array
@@ -12,8 +14,13 @@ public:
     Array(unsigned int size);
     Array(std::vector<unsigned int> shape);
     ~Array();
-    void print();
+    void print_flat();
+//    void print();
     void operator<< (T rhs);
+    int where(std::vector<unsigned int> coords);
+private:
+//    void print_helper();
+
 };
 
 template<typename T>
@@ -49,14 +56,35 @@ Array<T>::~Array()
     delete[] this->container;
 }
 template<typename T>
-void Array<T>::print()
+void Array<T>::print_flat()
 {
+    std::cout << "[";
     for (int i = 0; i < this->size; i++)
     {
-        std::cout << *(this->container + i) << " ";
+        std::cout << *(this->container + i);
+        if (i < this->size -1)
+        {
+            std::cout << ", ";
+        } else 
+        {
+            std::cout << "]" << std::endl;
+        }
     }
     std::cout << "\n";
 }
+/*
+template<typename T>
+void Array<T>::print_helper(std::array<unsigned int> this->dims> &tracker, unsigned int &row_counter)
+{
+    
+}
+template<typename T>
+void Array<T>::print()
+{
+    std::array<unsigned int> this->dims> tracker;
+    unsigned int row_counter = this->dims;
+}
+*/
 template<typename T>
 void Array<T>::operator <<(T rhs)
 {
@@ -64,4 +92,21 @@ void Array<T>::operator <<(T rhs)
     {
         *(this->container+i) = rhs;
     }
+}
+template<typename T>
+int Array<T>::where(std::vector<unsigned int> coords)
+{
+    if (coords.size() != this->dims)
+    {
+        throw std::invalid_argument("coordinate vector input must match num dimensions");
+    }
+    int index = 0;
+    for (int i = 0; i < this->dims; i++)
+    {
+        for (int j = this->dims -1; j > i; j--)
+        {
+            index += coords[i] * this->shape[j];
+        }
+    }
+    return index + coords[this->dims - 1];
 }
--- src/test.cpp
+++ src/test.cpp
@@ -4,10 +4,16 @@
 int main()
 {
     Array<int> a(10);
-    a.print();
+    a.print_flat();
     a << 1;
-    a.print();
-    Array<int> b({2,2,3});
-    b.print();
+    a.print_flat();
+    Array<int> b({3,2,2});
+    b.print_flat();
+
+    std::cout << "coordinate {0,0,0} (should be 0): " << b.where({0,0,0}) << std::endl;
+    std::cout << "coordinate {0,0,1} (should be 1): " << b.where({0,0,1}) << std::endl;
+    std::cout << "coordinate {0,1,0} (should be 2): " << b.where({0,1,0}) << std::endl;
+    std::cout << "coordinate {0,1,1} (should be 3): " << b.where({0,1,1}) << std::endl;
+    std::cout << "coordinate {2,0,0} (should be 8): " << b.where({2,0,0}) << std::endl;
     return 0;
 }
