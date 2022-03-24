842792a7681b188bc7d2879e5fce927f397ef283
Doug <akachi.sonne@gmail.com>
2022-03-23 20:49:04 -0500

    Added license and description in header.

--- src/array.hpp
+++ src/array.hpp
@@ -1,3 +1,35 @@
+/*
+ * -------------------------------------------------------------------------
+ * Copyright 2022 Douglas W. Palmer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -------------------------------------------------------------------------
+ */
+
+/* @file array.hpp
+ * @author Doug Palmer
+ * @version 1.0
+ *
+ * Description of class Array.
+ *
+ * Multidimensional array class template stored in contiguous memory on
+ * the heap.
+ *
+ * For usage, see the README.md file at: https://github.com/akachi-sonne/clin/blob/main/README.md
+ *
+ */
+
+
 // #define NDEBUG
 #include<iostream>
 #include<vector>
